d14b94124309d98cd50bf0bc03939b0241518d37
Edu Benito <eduardbenito@icloud.com>
2018-08-16 16:46:55 +0200

    few films route modifications

--- routes/films.js
+++ routes/films.js
@@ -7,15 +7,14 @@ const request = require('request');
 router.get('/search/:filter', (req, res, next) => {
   request(`${baseURL}${req.params.filter}`, (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      res.json(body);
+      //TODO return 'no-data' Movie not found
+      //control Too many results
+      //check year filter
+      res.json(JSON.parse(body));
     } else {
       res.status(404).json({code: 'not-found'});
     }
   })
-
-
-
-
 });
 
 module.exports = router;
