a930d22a53296282667d05e6d8beed16ef119c5d
Edu Benito <eduardbenito@icloud.com>
2018-08-19 19:31:54 +0200

    add film to favourites

--- models/film.js
+++ models/film.js
@@ -2,25 +2,19 @@ const mongoose = require("mongoose");
 const Schema   = mongoose.Schema;
 
 const filmSchema = new Schema({
-  imdbId: {
-    type:String,
+  imdbID: {
+    type: String,
     required: true
   },
-  title: {
-    type: String
-  },
-  year: {
-    type: Number
-  },
-  score: {
-    type: Number
-  },
-  reviews: {
-    type: mongoose.Schema.Types.ObjectId, ref: 'Review'
-  },
-  users: {
-    type: mongoose.Schema.Types.ObjectId, ref: 'User'
-  },
+  // title: {
+  //   type: String
+  // },
+  // year: {
+  //   type: Number
+  // },
+  score: Number,
+  reviews: [{type: mongoose.Schema.Types.ObjectId, ref: 'Review'}],
+  users: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
 }, {
   timestamps: true
 });
--- routes/films.js
+++ routes/films.js
@@ -25,47 +25,68 @@ router.get('/search/:filter', (req, res, next) => {
 });
 
 router.post('/addToFavourites/:params', (req, res, next) => {
-  const imdbId = '';
-  const title = '';
-  const year = 0;
+  let imdbID = '';
+  let userId = '';
   const score = 0;
-  const reviews = undefined;
-  const users = undefined;
+  const reviews = [];
+  const users = [];
   const params = req.params.params.split("&");
 
   const newFilm = Film({
-    imdbId,
-    title,
-    year,
+    imdbID,
     score,
     reviews,
     users
   });
 
-  request(`${baseURL}i=${params[0]}&type=movie`, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      if (JSON.parse(body).Error) {
-        console.log(JSON.parse(body).Error);
-      }
+  imdbID = params[0];
+  userId = params[1];
 
-      apiErrors = manageApiErrors(JSON.parse(body).Error);
-      if (apiErrors.status === 0) {
-        body = JSON.parse(body); 
-        newFilm.imdbId = body.imdbID;
-        newFilm.title = body.Title;
-        newFilm.year = body.Year;
-        newFilm.users = [params[1]];
-        newFilm.save()
-          .then(() => {
-            console.log('Film saved');
-          });
+  // Search if film already exists
+  Film.findOne({ imdbID })
+    .then((film) => {
+      if (film) {
+        // Find user in film users
+        if (film.users.findIndex(k => k==userId) === -1) {
+          // Add user to the film
+          film.users.push(userId);
+          film.save()
+            .then(() => {
+              console.log('User added to film');
+            });
+        }
       } else {
-        res.status(apiErrors.status).json({code: apiErrors.code});
+        // Create the film for the first time
+        request(`${baseURL}i=${imdbID}&type=movie`, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            if (JSON.parse(body).Error) {
+              console.log(JSON.parse(body).Error);
+            }
+      
+            apiErrors = manageApiErrors(JSON.parse(body).Error);
+            if (apiErrors.status === 0) {
+              const users = [];
+              users.push(userId);
+
+              body = JSON.parse(body); 
+              newFilm.imdbID = body.imdbID;
+              newFilm.score = 0;
+              newFilm.users = users;
+
+              newFilm.save()
+                .then(() => {
+                  console.log('Film saved');
+                });
+            } else {
+              res.status(apiErrors.status).json({code: apiErrors.code});
+            }
+          } else {
+            res.status(404).json({code: 'not-found'});
+          }
+        })
       }
-    } else {
-      res.status(404).json({code: 'not-found'});
-    }
-  })
+    })
+    .catch(next);
 })
 
 function manageApiErrors(error) {
